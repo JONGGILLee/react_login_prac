480193cb8fda4331764b2d56da9548b005fa926c
JONGGIL LEE <brianlee2k@naver.com>
2022-04-03 21:35:37 +0900

    1-9강 메인페이지(포스트목록)만들기 / props로 받아올 정보가 없을 때! 오류가 나거나 깨짐현상 -> Post.defaultProps ={  }

---
+++ src/components/Post.js
@@ -0,0 +1,33 @@
+// Post.js
+
+import React from "react";
+
+const Post = (props) => {
+  console.log(props);
+  return (
+    <React.Fragment>
+      <div>
+        <div>user profile / user name / insert_date / is_me (edit btn)</div>
+        <div>contents</div>
+        <div>image</div>
+        <div>comment count</div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+// props로 위의 정보들(use profile, name...) 받아올 때 주의점. props 정보가 없을때 화면 깨짐 등 오류
+Post.defaultProps = {
+  user_info: {
+    user_name: "JONGGIL",
+    user_profile:
+      "https://cdn.pixabay.com/photo/2022/03/27/18/00/feather-7095765__340.jpg",
+  },
+  image_url:
+    "https://cdn.pixabay.com/photo/2016/02/21/18/09/iceland-1214063__340.jpg",
+  contents: "저작권 없는 이미지파일입니다.",
+  comment_cnt: 15,
+  insert_dt: "2022-04-02 10:00:00",
+};
+
+export default Post;
